44acfca10df36d5cef645a8d839cd55c7c6ccd26
afeiship <1290657123@qq.com>
2021-09-01 10:41:47 +0800

    chore: add ref/desc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * @references
+ * https://stackoverflow.com/questions/4250364/how-to-trim-a-file-extension-from-a-string-in-javascript
+ *
+ * @description
+ * Strip the extension from a filename.
+ */
+
 const strip = (inFilename: string, inRecursive?: boolean): string => {
   var filename = inFilename;
   var recursive = inRecursive || false;
